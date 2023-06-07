4ada23cd907387d291afe0e514b0bdec35adfa70
Lechun <youlc1021@163.com>
2023-06-07 18:46:46 +0800

    update executor

--- executor.py
+++ executor.py
@@ -1,18 +1,49 @@
 from jina import DocumentArray, Executor, requests
 from InstructorEmbedding import INSTRUCTOR
+from typing import Dict
+import torch
 
 class InstructorEmbeddingExecutor(Executor):
     """InstructorEmbeddingExecutor embeds texts into 768-dim vectors using instructor embedding"""
-    def __init__(self, **kwargs):
+    def __init__(
+            self,
+            model_name: str = 'hkunlp/instructor-large',
+            batch_size: int = 32,
+            output_value: str = 'sentence_embedding',
+            convert_to_numpy: bool = True,
+            convert_to_tensor: bool = False,
+            normalize_embeddings: bool = False,
+            device: str = 'cpu',
+            **kwargs):
         super().__init__(**kwargs)
-        self.model = INSTRUCTOR('hkunlp/instructor-large')
+        self.batch_size = batch_size
+        self.output_value = output_value,
+        self.convert_to_numpy = convert_to_numpy,
+        self.convert_to_tensor = convert_to_tensor,
+        self.normalize_embeddings = normalize_embeddings,
+        self.model = INSTRUCTOR(model_name_or_path=model_name, device=device)
+
     @requests
     def foo(self, docs: DocumentArray, **kwargs):
         pass
 
     @requests(on='/')
-    def generate_embedding(self, docs: DocumentArray, **kwargs):
-        for i in docs:
-            for j in i.matches:
-                j.embedding = self.model.encode([[i.text, j.text]], convert_to_tensor=True)[0]
-        return docs
+    def encode(self, docs: DocumentArray, parameters: Dict = {}, **kwargs):
+        for doc in docs:
+            # doc.text : instruction
+            # doc.matches : sentences
+            document_batches_generator = DocumentArray(
+                filter(
+                    lambda d: d.text,
+                    doc.matches
+                )
+            ).batch(batch_size=parameters.get('batch_size', self.batch_size))
+
+            with torch.inference_mode():
+                for batch in document_batches_generator:
+                    batch.embeddings = self.model.encode(
+                        sentences=[[doc.text, j.text] for j in batch],
+                        output_value=parameters.get('output_value',self.output_value),
+                        convert_to_numpy=parameters.get('convert_to_numpy',self.convert_to_numpy),
+                        convert_to_tensor=parameters.get('convert_to_tensor',self.convert_to_tensor),
+                        normalize_embeddings=parameters.get('normalize_embeddings',self.normalize_embeddings))
